43de5d27c11061880f59704668215b3eaca15ed6
caprioara <dragos.neatax@gmail.com>
2021-02-25 15:27:27 +0200

    mute button + full screen

--- script.js
+++ script.js
@@ -2,28 +2,49 @@
 const videoContainer = document.querySelector('.video-container');
 const video = document.querySelector('.video-container video');
 
-const playButton = document.querySelector('.video-container .controls button.play');
+const playPauseButton = document.querySelector('.video-container .controls button.play-pause');
 const rewindButton = document.querySelector('.video-container .controls button.rewind');
 const fastForwardButton = document.querySelector('.video-container .controls button.fast-forward');
 const volumeButton = document.querySelector('.video-container .controls button.volume');
 const fullScreenButton = document.querySelector('.video-container .controls button.full-screen');
 
-const watchedBar = document.querySelector('.video-container .controls-container .progress-bar .watched-bar');
+const playButton = playPauseButton.querySelector('.playing');
+const pauseButton = playPauseButton.querySelector('.paused');
+const fullVolumeButton = volumeButton.querySelector('.full-volume')
+const mutedButton = volumeButton.querySelector('.muted')
+const maximizeButton = fullScreenButton.querySelector('.maximize');
+const minimizeButton = fullScreenButton.querySelector('.minimize');
 
+const progressBar = document.querySelector('.video-container .controls-container .progress-bar');
+const watchedBar = document.querySelector('.video-container .controls-container .progress-bar .watched-bar');
 const timeLeft = document.querySelector('.video-container .controls-container .time-remaining')
 
 watchedBar.style.width = '0px';
+pauseButton.style.display = 'none';
+minimizeButton.style.display = 'none';
 
 const playPause = () => {
     if(video.paused) {
         video.play();
+        playButton.style.display = 'none';
+        pauseButton.style.display = '';
     } else {
         video.pause();
+        playButton.style.display = '';
+        pauseButton.style.display = 'none';
     }
 }
 
 const toggleMute = () => {
     video.muted = !video.muted;
+    if (video.muted) {
+        fullVolumeButton.style.display = 'none';
+        mutedButton.style.display = '';
+    } else {
+        fullVolumeButton.style.display = '';
+        mutedButton.style.display = 'none';
+    }
+    
 }
 
 document.addEventListener('keydown', (event) => {
@@ -56,7 +77,12 @@ video.addEventListener('timeupdate', () => {
 
 });
 
-playButton.addEventListener('click', playPause);
+progressBar.addEventListener('click', (event) => {
+    const pos = (event.pageX  - (progressBar.offsetLeft + progressBar.offsetParent.offsetLeft)) / progressBar.offsetWidth;
+    video.currentTime = pos * video.duration;
+});
+
+playPauseButton.addEventListener('click', playPause);
 
 rewindButton.addEventListener('click', () => {
     video.currentTime -= 10;
@@ -71,8 +97,12 @@ volumeButton.addEventListener('click', toggleMute);
 fullScreenButton.addEventListener('click', () => {
     if(!document.fullscreenElement) {
         videoContainer.requestFullscreen();
+        maximizeButton.style.display = 'none';
+        minimizeButton.style.display = '';
     } else {
         document.exitFullscreen();
+        maximizeButton.style.display = '';
+        minimizeButton.style.display = 'none';
     }
 });
 
